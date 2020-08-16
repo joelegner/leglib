edecfe4ece63a48ec4f47de1444764571dc18ea5
Joe Legner <joelegner.gmail.com>
2020-08-16 15:56:39 -0400

    Moved all to temporary leglib-old directory

--- leglib-old/reportmaker.py
+++ leglib-old/reportmaker.py
@@ -4,8 +4,10 @@ from leglib import fmt
 from leglib import latex
 from jinja2.utils import contextfunction
 import numbers
+import math
 
-searchpath=["./", "/media/family/GoogleOne/Tools/"]
+searchpath = ["./", "/media/family/GoogleOne/Tools/"]
+
 
 def _get_context(locals, names):
     name_set = set(locals.keys()).intersection(names)
@@ -22,16 +24,19 @@ def make_report(locals, names, template_filename):
     context = _get_context(locals, names)
     make_report_from_context(context, template_filename)
 
+
 def fixed3(value):
     return fmt.fixed(value, 3)
+
 
 @contextfunction
 def formula(context, answervar, eq, units=""):
     #import pdb; pdb.set_trace()
 
-    vars = [symb for symb in list(context.keys()) if len(symb) <= 4 and isinstance(context[symb], numbers.Number)]
+    vars = [symb for symb in list(context.keys()) if len(
+        symb) <= 4 and isinstance(context[symb], numbers.Number)]
     latex_vars = []
-    vars.sort(key=len, reverse=True) # sorts by descending length
+    vars.sort(key=len, reverse=True)  # sorts by descending length
     eq0 = eq
     latex_answervar = answervar
     for var in vars:
@@ -49,19 +54,23 @@ def formula(context, answervar, eq, units=""):
         name = lv[1]
         val = fmt.sigdig(context[lv[0]], 4)
         eq = eq.replace(name, "(%s)" % val)
-    eq = "$" + latex_answervar + " = " + eq0 + " = " + eq + " = %s$ " % fmt.sigdig(context[answervar], 3) + units
+    eq = "$" + latex_answervar + " = " + eq0 + " = " + eq + \
+        " = %s$ " % fmt.sigdig(context[answervar], 3) + units
 #    import pdb; pdb.set_trace()
     return eq
 
+
 def make_report_from_context(context, template_filename):
     templateLoader = jinja2.FileSystemLoader(searchpath=searchpath)
-    templateEnv = jinja2.Environment(loader=templateLoader)
+    templateEnv = jinja2.Environment(
+        loader=templateLoader, comment_start_string="<!--", comment_end_string="-->")
     templateEnv.globals.update(f=formula)
     templateEnv.filters["s"] = fmt.sigdig
     templateEnv.filters["f"] = fmt.fixed
     templateEnv.filters["f3"] = fixed3
     templateEnv.filters["i"] = fmt.integer
     templateEnv.filters["in"] = fmt.inches_decimal
+    templateEnv.filters["int"] = int
     template = templateEnv.get_template(template_filename)
     outputText = template.render(context)
     print(outputText)
